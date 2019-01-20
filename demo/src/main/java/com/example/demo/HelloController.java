c7742845cbb260d02d13981898c91e9b87b043e2
welentsai <welentsai@gmail.com>
2019-01-20 10:11:36 +0800

    add up a JSON exmaple in controller

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
@@ -22,6 +25,14 @@ public class HelloController {
     @GetMapping("/greeting/{name}")
     String greetingByName(@PathVariable String name) {
       return "Hello " + name + ", greeting from Spring Boot";
+    }
+
+    // 範例, 回傳JSON
+    @GetMapping("/hello")
+    public Map hello() {
+      Map map = new HashMap();
+      map.put("say", "hello");
+      return map;
     }
 
 }
